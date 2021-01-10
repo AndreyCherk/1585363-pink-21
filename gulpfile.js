68996f701b9086e16c5854336a628a32bb7888ab
AndreyCher <forward14@bk.ru>
2021-01-10 17:31:48 +0300

    Доверстал страницу формы

--- gulpfile.js
+++ gulpfile.js
@@ -130,11 +130,19 @@ const server = (done) => {
 
 exports.server = server;
 
+// Reload
+
+const reload = done => {
+  sync.reload();
+  done();
+}
+
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html", gulp.series(html, sync.reload));
+  gulp.watch("source/less/**/*.less", gulp.series(styles));
+  gulp.watch("source/js/script.js", gulp.series(scripts));
+  gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
 // Build
