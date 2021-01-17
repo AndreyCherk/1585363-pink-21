e34c94578d4ef635be34349d50741b1722c8518d
AndreyCher <forward14@bk.ru>
2021-01-17 16:13:47 +0300

    завершающие доработки

--- source/js/script.js
+++ source/js/script.js
@@ -22,10 +22,10 @@ var navMain = document.querySelector('.main-nav');
   let sliderButtons, priceTable;
 
   if (priceBlock) {
-    const sliderControls = priceBlock.querySelector('.slider__toggles');
+    const sliderControls = priceBlock.querySelector('.reviews-toggles');
     priceTable = priceBlock.querySelector('.price__table');
 
-    sliderButtons = Array.from(sliderControls.querySelectorAll('.slider__toggle'));
+    sliderButtons = Array.from(sliderControls.querySelectorAll('.reviews-toggles__toggle'));
 
     if (sliderButtons) {
       sliderControls.addEventListener('click', clickSliderControls);
@@ -33,19 +33,104 @@ var navMain = document.querySelector('.main-nav');
   }
 
   function findCurrentButton(element) {
-    return element.classList.contains('slider__toggle--current');
+    return element.classList.contains('reviews-toggles__toggle--current');
   }
 
   function clickSliderControls(evt) {
     let element = evt.target;
 
-    if (element.classList.contains('slider__toggle')) {
+    if (element.classList.contains('reviews-toggles__toggle')) {
       let indexCurrentButton = sliderButtons.indexOf(sliderButtons.find(findCurrentButton));
       let indexNextButton = sliderButtons.indexOf(element);
 
-      sliderButtons[indexCurrentButton].classList.remove('slider__toggle--current');
-      sliderButtons[indexNextButton].classList.add('slider__toggle--current');
+      sliderButtons[indexCurrentButton].classList.remove('reviews-toggles__toggle--current');
+      sliderButtons[indexNextButton].classList.add('reviews-toggles__toggle--current');
       priceTable.style.left = `$(150 - indexNextButton * 100)%`;
+    }
+  }
+})();
+
+// reviews-slider
+(function () {
+  const reviewsSlider = document.getElementById('reviews');
+  let sliderButtons, sliderNavButtons, slides;
+
+  if (reviewsSlider) {
+    const sliderControls = reviewsSlider.querySelector('.reviews-toggles');
+    const sliderNav = reviewsSlider.querySelector('.slider-buttons');
+
+    sliderButtons = Array.from(sliderControls.querySelectorAll('.reviews-toggles__toggle'));
+    sliderNavButtons = Array.from(sliderNav.querySelectorAll('.slider-buttons__button'));
+    slides = Array.from(reviewsSlider.querySelectorAll('.reviews__list'));
+
+    if (slides && sliderButtons) {
+      sliderControls.addEventListener('click', clickSliderControls);
+      sliderNav.addEventListener('click', clickSliderNav);
+    }
+  }
+
+  function findCurrentButton(element) {
+    return element.classList.contains('reviews-toggles__toggle--current');
+  }
+
+  function findCurrentSlide(element) {
+    return element.classList.contains('reviews__list--current');
+  }
+
+  function changeSlides(indexCurrentSlide, indexNextSlide) {
+    slides[indexCurrentSlide].classList.remove('reviews__list--current');
+    slides[indexNextSlide].classList.add('reviews__list--current');
+  }
+
+  function clickSliderControls(evt) {
+    let element = evt.target;
+
+    if (element.classList.contains('reviews-toggles__toggle')) {
+      evt.preventDefault();
+
+      let indexCurrentButton = sliderButtons.indexOf(sliderButtons.find(findCurrentButton));
+      let indexNextButton = sliderButtons.indexOf(element);
+
+      sliderButtons[indexCurrentButton].classList.remove('reviews-toggles__toggle--current');
+      sliderButtons[indexNextButton].classList.add('reviews-toggles__toggle--current');
+
+      let indexCurrentSlide = slides.indexOf(slides.find(findCurrentSlide));
+      let indexNextSlide = indexNextButton;
+
+      changeSlides(indexCurrentSlide, indexNextSlide);
+    }
+  }
+
+  function clickSliderNav(evt) {
+    let element = evt.target;
+
+    if (element.classList.contains('slider-buttons__button')) {
+      evt.preventDefault();
+
+      let indexButton = sliderNavButtons.indexOf(element);
+      let indexCurrentSlide = slides.indexOf(slides.find(findCurrentSlide));
+      let indexNextSlide = indexCurrentSlide;
+
+      switch (indexButton) {
+        case 0:
+          indexNextSlide -= 1;
+          if (indexNextSlide < 0) {
+            indexNextSlide = slides.length - 1;
+          }
+          break;
+
+        case 1:
+          indexNextSlide += 1;
+          if (indexNextSlide >= slides.length) {
+            indexNextSlide = 0;
+          }
+          break;
+
+        default:
+          break;
+      }
+
+      changeSlides(indexCurrentSlide, indexNextSlide);
     }
   }
 })();
